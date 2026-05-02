d5d86d516496844f92ab4fb0daa0332fe33e4fe8
douaaelarif12 <elarifdouaa@gmail.com>
2026-05-02 21:04:05 +0100

    Update Sandokti project

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 ﻿import 'dart:async' show unawaited;
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 import 'app/navigation/auth_gate.dart';
+import 'app/theme/theme_provider.dart';
 import 'features/budget/data/datasources/firestore/sandokti_firestore.dart';
 import 'firebase_options.dart';
 
@@ -23,14 +25,32 @@ Future<void> main() async {
   runApp(const ProviderScope(child: SandoktiApp()));
 }
 
-class SandoktiApp extends StatelessWidget {
+class SandoktiApp extends ConsumerWidget {
   const SandoktiApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeModeProvider);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Sandokti',
+      themeMode: themeMode,
+
+      theme: ThemeData(
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: const Color(0xFFFFF7FC),
+        cardColor: Colors.white,
+        useMaterial3: true,
+      ),
+
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xFF0F172A),
+        cardColor: const Color(0xFF1E293B),
+        useMaterial3: true,
+      ),
+
       home: const AuthGate(),
     );
   }
